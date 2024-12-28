8634bc1aa14f23e74078bffd7a585f3ba0b99f9b
sofyan <sofyanbabar@gmail.com>
2024-12-28 23:37:38 +0500

    Added my project

---
+++ 01basics/src/App.jsx
@@ -0,0 +1,15 @@
+import Dec from './assets/dec'
+
+function App() {
+  
+
+  return (
+    <>
+    <Dec />
+      <h1>chai aur december </h1>
+        
+    </>
+  )
+}
+
+export default App
---
+++ 01basics/src/assets/dec.jsx
@@ -0,0 +1,24 @@
+function Dec() {
+  return(
+
+
+
+    <>
+    
+    <h1>here are some rules </h1>
+
+    <ul>
+
+        <li>firts of all use capital letter for function name anywaays it throughs an error </li>
+<li> second their is only sigle index file in react and we change roots ion that single page for expale cotact page 
+    aaboutpage etc for that we use script  </li>
+        <li>also use capital letters for tthe file name and in vite use jsx </li>
+    </ul>
+    
+    
+    
+    
+    </>
+  ) ;
+}
+export default Dec;
